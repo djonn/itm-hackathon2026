ec82daae8219e72cfd532464bbc8366983fcd447
Jonas Nielsen <djonnneller@gmail.com>
2026-05-01 23:31:28 +0200

    Action based on category

--- pokerchamp/FlopOn.cs
+++ pokerchamp/FlopOn.cs
@@ -4,10 +4,30 @@ public class FlopOn
 {
     public static (string Action, int? Amount) DecideAction(PokerMind.Client.Model.Game game)
     {
+        var (myRanking, myCategory) = rankHand(game);
+
+        return myCategory switch
+        {
+            PokerHandCategory.HighCard => ("fold", null),
+            PokerHandCategory.OnePair => ("call", null),
+            PokerHandCategory.TwoPairs => ("call", null),
+            PokerHandCategory.ThreeOfAKind => ("raise", game.RaiseAmount),
+            PokerHandCategory.Straight => ("raise", game.RaiseAmount * 2),
+            PokerHandCategory.Flush => ("raise", game.RaiseAmount * 3),
+            PokerHandCategory.FullHouse => ("raise", game.RaiseAmount * 4),
+            PokerHandCategory.FourOfAKind => ("all_in", null),
+            PokerHandCategory.StraightFlush => ("all_in", null),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    private static (int, PokerHandCategory) rankHand(PokerMind.Client.Model.Game game)
+    {
         var playerHand = game.Player.CurrentHand.Select(c => c.ToNuget()).ToArray();
         var communityCards = game.CommunityCards.Select(c => c.ToNuget()).ToArray();
 
         int ranking = HoldemHandEvaluator.GetHandRanking([..playerHand, ..communityCards]);
+        var category = HoldemHandEvaluator.GetHandCategory(ranking);
 
         var log = $"""
         {string.Join("\n", playerHand.Select(c => Formatter.FormatCard(c.ToClient())))}
@@ -15,10 +35,11 @@ public class FlopOn
         {string.Join("\n", communityCards.Select(c => Formatter.FormatCard(c.ToClient())))}
 
         Hand ranking: {ranking}
+        Hand category: {category}
         """;
 
         Console.WriteLine(log);
 
-        return ("check", null);
+        return (ranking, category);
     }
 }
